b29a68d8bc8c8bcb139bffacee86c5cb1a7d227b
zackattacknyu <zackattacknyu@gmail.com>
2015-05-07 13:00:55 -0700

    more work done on prob1

--- Homework2/prob1script.m
+++ Homework2/prob1script.m
@@ -1,21 +1,40 @@
-I = imread('squirtle1.JPG');
-I = single(rgb2gray(I));
-[f,d] = vl_sift(I);
-%%
-perm = randperm(size(f,2)) ;
-sel = perm(1:50) ;
+
 
 %%
 
 %visualizes points
+I1 = imread('squirtle1.JPG');
+I1 = single(rgb2gray(I1));
+[f1,d1] = vl_sift(I1);
+
+I2 = imread('squirtle2.JPG');
+I2 = single(rgb2gray(I2));
+[f2,d2] = vl_sift(I2);
+
+perm = randperm(size(f1,2)) ;
+sel1 = perm(1:50) ;
+perm = randperm(size(f2,2)) ;
+sel2 = perm(1:50) ;
+
 figure
-imagesc(I);
+subplot(1,2,1)
+imagesc(I1);
 colormap bone;
-h1 = vl_plotframe(f(:,sel)) ;
-h2 = vl_plotframe(f(:,sel)) ;
+h1 = vl_plotframe(f1(:,sel1)) ;
+h2 = vl_plotframe(f1(:,sel1)) ;
 set(h1,'color','k','linewidth',3) ;
 set(h2,'color','y','linewidth',2) ;
-h3 = vl_plotsiftdescriptor(d(:,sel),f(:,sel)) ;
+h3 = vl_plotsiftdescriptor(d1(:,sel1),f1(:,sel1)) ;
+set(h3,'color','g') ;
+
+subplot(1,2,2)
+imagesc(I2);
+colormap bone;
+h1 = vl_plotframe(f2(:,sel1)) ;
+h2 = vl_plotframe(f2(:,sel1)) ;
+set(h1,'color','k','linewidth',3) ;
+set(h2,'color','y','linewidth',2) ;
+h3 = vl_plotsiftdescriptor(d2(:,sel1),f2(:,sel1)) ;
 set(h3,'color','g') ;
 
 %%
