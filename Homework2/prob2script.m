7372c90ac03675d558634bbfaefbb903bf5ec148
zackattacknyu <zackattacknyu@gmail.com>
2015-05-09 18:24:35 -0700

    started prob 2

---
+++ Homework2/prob2script.m
@@ -0,0 +1,13 @@
+%{
+
+For this problem, estimate the fundamental matrix
+    x'Fx
+by solving that linear equation
+
+Use RANSAC algorithm to find an inital F and then update it
+
+Code for RANSAC:
+http://www.cb.uu.se/~aht/code.html
+
+
+%}
