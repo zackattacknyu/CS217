87db7107f0727c7f9871809319bf69a91813e9eb
zackattacknyu <zackattacknyu@gmail.com>
2015-05-10 19:46:11 -0700

    got example pictures for textbook

--- Homework2/prob1script.m
+++ Homework2/prob1script.m
@@ -1,20 +1,17 @@
-
-
-%%
-
-imageName = 'squirtle';
+%specify the image in the folder
+imageName = 'book';
 image1 = strcat(imageName,'1.JPG');
 image2 = strcat(imageName,'2.JPG');
 
-%visualizes points
+%run SIFT on image1 and image2
 I1 = imread(image1);
 I1 = single(rgb2gray(I1));
 [f1,d1] = vl_sift(I1);
-
 I2 = imread(image2);
 I2 = single(rgb2gray(I2));
 [f2,d2] = vl_sift(I2);
 
+%select random subsets fo the points
 perm = randperm(size(f1,2)) ;
 sel1 = perm(1:50) ;
 perm = randperm(size(f2,2)) ;
@@ -23,10 +20,10 @@ sel2 = perm(1:50) ;
 %does basic matching
 [matches, scores] = vl_ubcmatch(d1, d2) ;
 
-%%
+%goes through the points visualized in sel1 and sel2
+%   and finds their corresponding matches in the other images
 image1matches = matches(1,:);
 image2matches = matches(2,:);
-
 numPoints = size(sel1,2);
 image1Points = [];
 image2Points = [];
@@ -53,12 +50,13 @@ for i = 1:numPoints
     end
 end
 %%
+%plots the left and right image side-by-side
 doubleImage = [I1 I2];
 figure
 imagesc(doubleImage);
 colormap bone;
 
-%plots the results for the left image
+%plots the SIFT points for left image
 h1 = vl_plotframe(f1(:,sel1)) ;
 h2 = vl_plotframe(f1(:,sel1)) ;
 set(h1,'color','k','linewidth',3) ;
@@ -66,7 +64,7 @@ set(h2,'color','y','linewidth',2) ;
 h3 = vl_plotsiftdescriptor(d1(:,sel1),f1(:,sel1)) ;
 set(h3,'color','g') ;
 
-%plots the results for the right image
+%plots the SIFT points for the right image
 width = size(I1,2);
 f2Original = f2(:,sel2);
 f2Offset = repmat([width;0;0;0],1,50);
@@ -78,6 +76,7 @@ set(h2,'color','y','linewidth',2) ;
 h3 = vl_plotsiftdescriptor(d2(:,sel2),f2New) ;
 set(h3,'color','g') ;
 %%
+%plots the matches for the visualized points in each image
 hold on
 for i = 1:size(image1Points,2)
    Xvals = [image1Points(1,i) image2Points(1,i)+width];
